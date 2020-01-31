51b97bc29c35dac94b25731b47b323d4cfdb1ac6
Lee Martin <lee.martin@pliminus.co.uk>
2020-01-31 23:46:02 +0000

    Hole Generation based on triangulator

--- Assets/Scripts/LeftSide/Hole.cs
+++ Assets/Scripts/LeftSide/Hole.cs
@@ -14,30 +14,50 @@ public class Hole : MonoBehaviour
 {
     public Material material;
 
-    public float width = 1;
-    public float height = 1;
+    float width = 1;
+    float height = 1;
 
     public Shape shapeType = Shape.Triangle;
 
     // Start is called before the first frame update
     void Start()
     {
-        Mesh mesh = new Mesh();
+        Mesh mesh;
 
-        Vector3[] vertices = new Vector3[4];
-
-        vertices[0] = new Vector3(-width, -height);
-        vertices[1] = new Vector3(-width, height);
-        vertices[2] = new Vector3(width, height);
-        vertices[3] = new Vector3(width, -height);
-
-        mesh.vertices = vertices;
-
-        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        switch(shapeType){
+            case Shape.Triangle:
+            default:
+                mesh = GenerateTriangle();
+                break;
+        }
 
         GetComponent<MeshRenderer>().material = material;
 
         GetComponent<MeshFilter>().mesh = mesh;
+    }
+    
+    Mesh GenerateTriangle()
+    {
+        Mesh mesh = new Mesh();
+        Vector2[] vertices2D = new Vector2[3];
+
+        vertices2D[0] = new Vector2(-width, -height);
+        vertices2D[1] = new Vector2(-width, height);
+        vertices2D[2] = new Vector2(width, height);
+
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+
+        Vector3[] vertices = new Vector3[vertices2D.Length];
+        for (int i=0; i<vertices.Length; i++) {
+            vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        return mesh;
     }
 
     // Update is called once per frame
