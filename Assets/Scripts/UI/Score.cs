0a96e43973654875ca9eb63fa320924963fce520
Lee Martin <lee.martin@pliminus.co.uk>
2020-02-02 12:05:04 +0000

    Star rating system

--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public int StarStart = 0;
+    public int StarEnd = 0;
     private void Update()
     {
+        
         if(GameManager.Instance.Score != 0){
-            GetComponent<Text>().text = GameManager.Instance.Score.ToString();
+            if(GameManager.Instance.Score > StarEnd){
+                GetComponent<Image>().fillAmount = 1f;
+            } else {
+                int remaining = GameManager.Instance.Score - StarStart;
+                float fraction = (float)remaining / (float)(StarEnd - StarStart);
+                GetComponent<Image>().fillAmount = fraction;
+
+            }
         } else {
-            GetComponent<Text>().text = "";
+            GetComponent<Image>().fillAmount = 0f;
         }
     }
 }
