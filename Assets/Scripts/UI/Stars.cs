661207b369bccf4e60e520689ed994b1ed5d1a68
Lee Martin <lee.martin@pliminus.co.uk>
2020-02-02 12:12:24 +0000

    missing images

---
+++ Assets/Scripts/UI/Stars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour
+{
+ 
+    private void Start()
+    {
+        GameManager.Instance.RegisterGameOver(GameOver);
+        GameManager.Instance.RegisterBeforeTutorial(Restart);
+
+        Restart();
+    }
+
+    private void Restart()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void GameOver()
+    {
+        gameObject.SetActive(true);
+    }
+}
